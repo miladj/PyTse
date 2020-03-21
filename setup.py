372c9252b3af02bda865e2d1aaa760335e9f553b
Milad <miladj>
2020-03-21 16:35:26 +0430

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from pathlib import Path
 setuptools.setup(
     name="pytse",
-    version="1.0.post5",
+    version="1.0.1",
     long_description=Path("README.md").read_text(),
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(exclude=["tests","data"]),

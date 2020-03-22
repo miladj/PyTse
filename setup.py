a9f95fb3a4aa9ddf4113644dd1acfdaa949631ef
miladj <miladj@users.noreply.github.com>
2020-03-22 21:36:16 +0430

    add description to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version="1.0.1",
     long_description=Path("README.md").read_text(),
     long_description_content_type='text/markdown',
+    description="A small web crawler for tsetmc.com",
     packages=setuptools.find_packages(exclude=["tests","data"]),
     project_urls={
         'Source': 'https://github.com/miladj/PyTse',

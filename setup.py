d12ad898474cbab268c3e899e5110d1c517ad14b
miladj <miladj@users.noreply.github.com>
2020-04-09 11:17:35 +0430

    add new fields

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from pathlib import Path
 setuptools.setup(
     name="pytse",
-    version="1.1.0",
+    version="1.2.0",
     long_description=Path("README.md").read_text(),
     long_description_content_type='text/markdown',
     description="A small web crawler for tsetmc.com",

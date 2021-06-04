2c2c3af27f6d43c70e7f4f5fdf83b91fc9abace2
miladj <16033489+miladj@users.noreply.github.com>
2021-06-04 09:51:02 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from pytse.pytse import PyTse,SymbolData
+
+
+if(__name__=="__main__"):
+    PyTse.request_timeout = 20#changing timeout default=30 !apply to all requests
+    pytse=PyTse()#read_symbol_data=True,read_client_type=False
+    pytse.read_client_type() # در صورت نیاز به اطلاعات حقیقی
+    symbols=pytse.symbols_data
+    symbol=symbols["IRO1SIPA0001"]
+    symbol.fill_data() #درصورت نیاز به اطلاعات "میانگین حجم ماه "و "سهام شناور" فرخوانی شود
+    symbol_history_data=symbol.get_symbol_history()
+    print(symbol_history_data["20210106"])
+    print(symbol)
+    print(symbol.pl)
+    print(symbol.ct.Buy_CountI) #fill_data()
+    print(symbol["ct"]["Buy_CountI"]) #fill_data(
